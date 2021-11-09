a28f7f5312bd69144d5d41c6db00ad1ef5630f07
gop20 <gop20@inf.ufpr.br>
2021-11-09 11:05:45 -0300

    login screen

---
+++ lib/loginscreen.dart
@@ -0,0 +1,15 @@
+import 'dart:developer';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
