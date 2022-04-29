a826f61a239e086ec7dd86e03a23a08753a3d47f
cbravos5 <cnbravos5@gmail.com>
2022-04-28 21:59:56 -0300

    add estilizacao input e imagem

--- lib/widgets/test/test_animals.dart
+++ lib/widgets/test/test_animals.dart
@@ -24,8 +24,7 @@ List<_Cubes> cubeFaces2 = [
 
 class TestAnimals extends SuperTest {
   @override
-  final description =
-      "Digite o nome dos animais que aparecem na tela";
+  final description = "Digite o nome dos animais que aparecem na tela";
 
   const TestAnimals({Key? key}) : super(key: key);
 
@@ -50,15 +49,46 @@ class TestAnimalsState extends SuperTestState {
 
   @override
   Widget build(BuildContext context) => Scaffold(
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Image(image: AssetImage('assets/images/animals/borboleta.png')),
-            TextField(
-              
-            )
-          ]
-
-        )
-      );
+          body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                  height: MediaQuery.of(context).size.width * 0.65,
+                  decoration: BoxDecoration(
+                      border: Border.all(
+                          color: const Color(0xffe984b8), width: 2.5),
+                      borderRadius: BorderRadius.circular(5),
+                      image: const DecorationImage(
+                          image:
+                              AssetImage('assets/images/animals/borboleta.png'),
+                          fit: BoxFit.cover))),
+              Container(
+                margin: const EdgeInsets.fromLTRB(15, 20, 15, 0),
+                decoration: const BoxDecoration(
+                    color: Color(0xff00B3BE),
+                    borderRadius: BorderRadius.all(Radius.circular(8))),
+                child: const TextField(
+                  cursorColor: Color.fromARGB(17, 38, 38, 42),
+                  decoration: InputDecoration(
+                    fillColor: Colors.transparent,
+                    filled: true,
+                    border: InputBorder.none,
+                    floatingLabelBehavior: FloatingLabelBehavior.always,
+                    floatingLabelAlignment: FloatingLabelAlignment.center,
+                    label: Text(
+                      "DIGITE O NOME DO ANIMAL",
+                      style: TextStyle(
+                        fontSize: 20,
+                        height: 0.3,
+                        color: Color(0xff060607),
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ]),
+      ));
 }
