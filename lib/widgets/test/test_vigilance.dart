10e300a4e492d16f91eb75ff1f10eadeb955ac70
cbravos5 <cnbravos5@gmail.com>
2022-05-09 15:39:29 -0300

    feat: escolha de caracteres aleatorios com probabilidade

--- lib/widgets/test/test_vigilance.dart
+++ lib/widgets/test/test_vigilance.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:dezmente/super/supertest.dart';
 import 'package:flutter/material.dart';
 import 'dart:math';
@@ -12,17 +14,62 @@ class TestVigilance extends SuperTest {
   TestVigilanceState createState() => TestVigilanceState();
 }
 
+const chars = [
+  "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", //
+  "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" //
+];
+
+const probA = 0.35; // Probabilty of A being chosen
+const probOthers = 0.026; // Probabilty of other character being chosen
+const max = 5;
+
 class TestVigilanceState extends SuperTestState {
   @override
   erase() {}
 
+  String _char = "";
+  int _acertos = 0;
+  int _tries = 0;
+
   final _random = Random();
 
-  void setRandomImage() {
-    // int max = imageNames.length;
-    // int randomIndex = _random.nextInt(max - 0);
-    // imageNames.remove(_animal);
-    // controller.clear();
+  late Timer _timer;
+
+  void clearTimer() => _timer.cancel();
+
+  void setTimer() {
+    if (_tries == max) {
+      print(_acertos / max);
+      return;
+    }
+
+    setRandomChar();
+    _timer = Timer(const Duration(seconds: 3), () {
+      if (_char != "A") _acertos++;
+      setTimer();
+    });
+  }
+
+  void setRandomChar() {
+    _tries++;
+    int index = 0;
+    do {
+      index = 0;
+      double randomDouble = _random.nextDouble();
+      if (randomDouble > probA) {
+        index = ((randomDouble - probA) / probOthers).round();
+      }
+    } while (_char == chars[index]);
+
+    setState(() {
+      _char = chars[index];
+    });
+  }
+
+  @override
+  initState() {
+    super.initState();
+    setTimer();
   }
 
   int timeSpended = DateTime.now().millisecondsSinceEpoch;
@@ -38,14 +85,18 @@ class TestVigilanceState extends SuperTestState {
       child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text("A", style: TextStyle(fontSize: 128)),
-            SizedBox(height: 30),
+            Text(_char, style: TextStyle(fontSize: 116)),
+            const SizedBox(height: 30),
             ElevatedButton(
               child: const Text(
                 'CLIQUE',
                 style: TextStyle(fontSize: 24),
               ),
-              onPressed: () {},
+              onPressed: () {
+                clearTimer();
+                if (_char == "A") _acertos++;
+                setTimer();
+              },
               style: ButtonStyle(
                 fixedSize: MaterialStateProperty.all(const Size(125, 125)),
                 shape: MaterialStateProperty.all(const CircleBorder()),
