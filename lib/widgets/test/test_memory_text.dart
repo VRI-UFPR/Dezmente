916e4b6d1e68614fb66166c1e3d510c1278eb464
cbravos5 <cnbravos5@gmail.com>
2022-06-27 15:13:16 -0300

    fix: tamanho da fonte teste memoria de texto

--- lib/widgets/test/test_memory_text.dart
+++ lib/widgets/test/test_memory_text.dart
@@ -19,25 +19,28 @@ class TestMemoryTextState extends SuperTestState {
   erase() {}
 
   @override
-  Widget build(BuildContext context) =>
-      Scaffold(body: Center(child: SingleChildScrollView(child: _body())));
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    return Scaffold(
+        body: Center(child: SingleChildScrollView(child: _body(width))));
+  }
 
-  _body() {
+  _body(double width) {
     return Padding(
       padding: const EdgeInsets.all(20),
-      child: _buildText(),
+      child: _buildText(width),
     );
   }
 
-  Widget _buildText() => Container(
+  Widget _buildText(double width) => Container(
       padding: const EdgeInsets.all(10),
       decoration: BoxDecoration(
           color: const Color(0xffe984b8),
           border: Border.all(),
           borderRadius: BorderRadius.circular(10)),
-      child: const Text(
+      child: Text(
         "João combinou de ir à biblioteca municipal com o filho de seu tio no sábado. Antes de entrar no ônibus, percebeu que tinha esquecido o livro que havia emprestado em casa. Eles tiveram que voltar, pegaram o livro e atrasaram meia hora o passeio.",
         textAlign: TextAlign.justify,
-        style: TextStyle(fontSize: 28),
+        style: TextStyle(fontSize: width * 0.055),
       ));
 }
